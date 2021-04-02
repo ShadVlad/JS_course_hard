c63f76d268383e00e8085eb6e3dd46b97c92a3d8
Vladimir <wowchik.shadrin@yandex.ru>
2021-04-02 18:27:41 +0300

    homework lesson 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,20 @@
-let money;
-let incom;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let num = 266219;
 
-alert("Hello, teacher!");
-console.log("Это сообщение в консоли lesson02....");
+let numberOfString = num.toString();
+let lenght = numberOfString.length;
+let product = 1;
+for (var i = 0; i < lenght; i++) product *= numberOfString.charAt(i);
+
+console.log("Произведение цифр числа " + num + " равно " + product);
+let power3OfProduct = product ** 3;
+let power3OfProductStr = power3OfProduct.toString();
+console.log(product + " в степени 3  равно " + power3OfProduct);
+
+console.log(
+  "Первые цифры " +
+    power3OfProduct +
+    " равны " +
+    power3OfProductStr.toString().charAt(0) +
+    " и " +
+    power3OfProductStr.toString().charAt(1)
+);
