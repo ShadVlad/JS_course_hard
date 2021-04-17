7e0e198716664ce2061cbd761328fef3c76201aa
Vladimir <wowchik.shadrin@yandex.ru>
2021-04-17 19:45:46 +0300

    homework13 hard

--- scripts/script.js
+++ scripts/script.js
@@ -1,125 +1,18 @@
 "use strict";
-const dayOfWeek = document.getElementById("dayOfWeek");
-const outputA = document.createElement("p");
-const outputB = document.createElement("p");
-dayOfWeek.appendChild(outputA);
-dayOfWeek.appendChild(outputB);
-const updateDate = function () {
-  const week = [
-    "Воскресенье",
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-  ];
-  //const dayOfWeek = document.getElementById("dayOfWeek"); // Получаем div куда всё будем вставлять
-  let stringMonth = "";
-  const todayDay = new Date();
-  const year = todayDay.getFullYear();
-  const month = todayDay.getMonth();
-  const day = todayDay.getDate();
-  const hour = todayDay.getHours();
-  const minute = todayDay.getMinutes();
-  const second = todayDay.getSeconds();
-  const minuteString = ["минут", "минута", "минуты"],
-    hourString = ["часов", "час", "часа"],
-    secondString = ["секунд", "секунда", "секунды"];
+const color = document.getElementById("color");
+const btnNewColor = document.getElementById("change");
 
-  const declension = function (digits, arr) {
-    let digitsDiv10 = digits % 10;
-    let digitsDiv100 = digits % 100;
-    if (
-      (digitsDiv100 >= 11 && digits <= 19) ||
-      digitsDiv10 >= 5 ||
-      digitsDiv10 === 0
-    ) {
-      return arr[0];
-    } else if (digitsDiv10 === 1) {
-      return arr[1];
-    } else return arr[2];
-  };
-  const withZero = function (digit) {
-    return digit < 10 ? "0" + digit : digit;
-  };
-
-  switch (month) {
-    case 0:
-      stringMonth = "января";
-      break;
-    case 2:
-      stringMonth = "марта";
-      break;
-    case 1:
-      stringMonth = "февраля";
-      break;
-    case 3:
-      stringMonth = "апреля";
-      break;
-    case 4:
-      stringMonth = "мае";
-      break;
-    case 5:
-      stringMonth = "июня";
-      break;
-    case 6:
-      stringMonth = "июля";
-      break;
-    case 7:
-      stringMonth = "августа";
-      break;
-    case 8:
-      stringMonth = "сентября";
-      break;
-    case 9:
-      stringMonth = "октября";
-      break;
-    case 10:
-      stringMonth = "ноября";
-      break;
-    case 11:
-      stringMonth = "декабря";
-      break;
-  }
-  let stringA = `Сегодня  ${
-    week[todayDay.getDay()]
-  }, ${day} ${stringMonth} ${year} года, ${hour} ${declension(
-    hour,
-    hourString
-  )} ${minute} ${declension(minute, minuteString)} ${second} ${declension(
-    second,
-    secondString
-  )}`;
-
-  let stringB =
-    withZero(day) +
-    "." +
-    withZero(month + 1) +
-    "." +
-    withZero(year) +
-    " - " +
-    withZero(hour) +
-    ":" +
-    withZero(minute) +
-    ":" +
-    withZero(second);
-  outputA.innerHTML = stringA;
-  outputB.innerHTML = stringB;
+const changeColor = function () {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  let colorValue = "#" + r.toString(16) + g.toString(16) + b.toString(16);
+  console.log("colorValue: ", colorValue);
+  color.innerHTML = colorValue;
+  document.body.style.backgroundColor = colorValue;
+  //console.log("body: ", body);
 };
-setInterval(updateDate, 1000);
-// week.forEach((item, i) => {
-//   // Для каждого дня недели создаем блок
-//   let newDay = document.createElement("p");
-//   // если текущий день для привязки стилей
-//   if (i == todayDay.getDay()) {
-//     newDay.classList.add("today");
-//   }
-//   // Если выходные то
-//   if (item == "Суббота" || item == "Воскресенье") {
-//     newDay.classList.add("weekend");
-//   }
-//   newDay.textContent = week[i];
 
-//   dayOfWeek.appendChild(newDay);
-// });
+btnNewColor.addEventListener("click", changeColor);
+
+//const dayOfWeek = document.getElementById("dayOfWeek"); // Получаем div куда всё будем вставлять
